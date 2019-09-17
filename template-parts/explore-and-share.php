5b89e54146742226d5241d0319acf83b7eb43d24
ksascomm <ksasweb@jhu.edu>
2019-09-17 15:36:44 -0400

    v.2.3.1: accessiblity fixes

--- template-parts/explore-and-share.php
+++ template-parts/explore-and-share.php
@@ -24,10 +24,10 @@
 				<ul class="menu simple">
 					<li class="menu-text">Share This Story:</li>
 					<li>
-						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>" target="_blank"><span class="fab fa-facebook fa-2x"></span><span class="screen-reader-text">Facebook</span></a>
+						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>" target="_blank" aria-label="Share this on Facebook"><span class="fab fa-facebook fa-2x"></span><span class="screen-reader-text">Facebook</span></a>
 					</li>	
 					<li>
-						<a href="https://twitter.com/intent/tweet?text=<?php echo urlencode( get_the_title() ) ?>&amp;url=<?php echo urlencode( get_permalink() ); ?>&amp;via=JHUArtsSciences" target="_blank" title="Share this on Twitter"><span class="fab fa-twitter fa-2x"></span><span class="screen-reader-text">Twitter</span></a>
+						<a href="https://twitter.com/intent/tweet?text=<?php echo urlencode( get_the_title() ) ?>&amp;url=<?php echo urlencode( get_permalink() ); ?>&amp;via=JHUArtsSciences" target="_blank" aria-label="Share this on Twitter"><span class="fab fa-twitter fa-2x"></span><span class="screen-reader-text">Twitter</span></a>
 					</li>
 					<li>
 						<a href="#" data-open="exampleModal1"><span class="fa fa-envelope fa-2x"></span><span class="screen-reader-text">Email</span></a>
