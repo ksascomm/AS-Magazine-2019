a8db1e1a8a1f1cdb33ee08ea64ec2d5bd20cd697
ksascomm <ksasweb@jhu.edu>
2019-10-18 13:56:42 -0400

    add external css back

--- header.php
+++ header.php
@@ -15,6 +15,8 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php wp_head(); ?>
+		<!--Kathy css edits. Bundle back into /assets-->
+		<link rel="stylesheet" href="https://dl.dropbox.com/s/mznfcnfrb69cswq/kathy.css"  type="text/css">		
 	</head>
 	<body <?php body_class(); ?>>
 		
