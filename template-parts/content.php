538973821a91a4e3eddb625764d11a9b8f77a092
ksascomm <ksasweb@jhu.edu>
2019-10-28 14:53:48 -0400

    supress meta on dean category

--- template-parts/content.php
+++ template-parts/content.php
@@ -28,7 +28,9 @@ $volume_name = get_the_volume_name($post);
 	?>
 	<div class="entry-meta">
 		<p class="byline issue"><?php echo $volume_name; ?></p>
-		<?php foundationpress_entry_meta(); ?>
+		<?php if (!has_category('dean')):?>
+			<?php foundationpress_entry_meta(); ?>
+		<?php endif;?>
 	</div>
 	<?php if ( function_exists('get_field') && get_field( 'show_featured_image' ) == 1 ) {
 			get_template_part( 'template-parts/featured-image' ); 
