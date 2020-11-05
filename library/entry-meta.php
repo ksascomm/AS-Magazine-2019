1e9d4a29448826047274e5f7e0b6c13c009f1346
ksascomm <ksascomm@gmail.com>
2020-11-05 13:08:04 -0500

    Add publishpress author plugin functionality

--- library/entry-meta.php
+++ library/entry-meta.php
@@ -23,22 +23,27 @@ if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 		elseif ($author == '5') {
 			echo '<p class="byline author">' . __( 'By Magazine Staff', 'foundationpress' ) . '</p>';
 
-		}				
+		}
 		//Morgan
 		elseif ($author == '151') {
 			echo '<p class="byline author">' . __( 'By Magazine Staff', 'foundationpress' ) . '</p>';
 
 		}
-		//Amanda
-		elseif ($author == '228') {
+		//Rebecca
+		elseif ($author == '378') {
 			echo '<p class="byline author">' . __( 'By Magazine Staff', 'foundationpress' ) . '</p>';
 
 		}
 		else {
-			echo '<p class="byline author">' . __( 'By', 'foundationpress' ) . ' <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" rel="author" class="fn">' . get_the_author() . '</a></p>';
-			/* translators: %1$s: current date, %2$s: current time */
-			//echo '<time class="updated" datetime="' . get_the_time( 'c' ) . '">' . sprintf( __( 'Posted on %1$s at %2$s.', 'foundationpress' ), get_the_date(), get_the_time() ) . '</time>';
-			
-		} 
+				if ( is_plugin_active('publishpress-authors-pro/publishpress-authors-pro.php') ) {
+					echo '<p class="byline author">' . __( 'By', 'foundationpress' );
+					do_action( 'pp_multiple_authors_show_author_box', false, 'inline' );
+					echo '</p>';
+				} else {
+					echo '<p class="byline author">' . __( 'By', 'foundationpress' ) . ' <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" rel="author" class="fn">' . get_the_author() . '</a></p>';
+					/* translators: %1$s: current date, %2$s: current time */
+					//echo '<time class="updated" datetime="' . get_the_time( 'c' ) . '">' . sprintf( __( 'Posted on %1$s at %2$s.', 'foundationpress' ), get_the_date(), get_the_time() ) . '</time>';
+				}
+		}
 	}
 endif;
