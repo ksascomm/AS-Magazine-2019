5a9b1c2ad62520102466bffb8f2705ebd785ae5a
ksascomm <ksascomm@gmail.com>
2020-11-11 14:17:17 -0500

    Update Gutenberg Styles

--- library/gutenberg.php
+++ library/gutenberg.php
@@ -3,6 +3,9 @@
 if ( ! function_exists( 'foundationpress_gutenberg_support' ) ) :
 	function foundationpress_gutenberg_support() {
 
+		// Enable widge alignments
+		add_theme_support( 'align-wide' );
+
     // Add foundation color palette to the editor
     add_theme_support( 'editor-color-palette', array(
         array(
@@ -21,3 +24,27 @@ if ( ! function_exists( 'foundationpress_gutenberg_support' ) ) :
 
 	add_action( 'after_setup_theme', 'foundationpress_gutenberg_support' );
 endif;
+
+/**
+ * Gutenberg scripts
+ * @link https://www.billerickson.net/block-styles-in-gutenberg/
+ */
+function custom_gutenberg_scripts() {
+	wp_enqueue_script(
+		'custom-editor',
+		get_stylesheet_directory_uri() . '/gutenberg-editor/editor.js',
+		array( 'wp-blocks', 'wp-dom' ),
+		filemtime( get_stylesheet_directory() . '/gutenberg-editor/editor.js' ),
+		true
+	);
+}
+add_action( 'enqueue_block_editor_assets', 'custom_gutenberg_scripts' );
+
+add_action( 'after_setup_theme', 'custom_gutenberg_css' );
+
+function custom_gutenberg_css(){
+
+	add_theme_support( 'editor-styles' ); // if you don't add this line, your stylesheet won't be added.
+	add_editor_style( 'gutenberg-editor/editor-style.css' ); // tries to include editor-style.css directly from your theme folder.
+
+}
