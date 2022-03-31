94a2feafe34f75cf607aca3501c6617853f671dd
ksascomm <ksasweb@jhu.edu>
2022-03-31 10:15:22 -0400

    Remove tracking codes
    Codes available in GTM

--- header.php
+++ header.php
@@ -33,22 +33,6 @@
 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 		})(window,document,'script','dataLayer','GTM-T9S2PC4');</script>
 		<!-- End Google Tag Manager -->
-		<!-- Facebook Pixel Code -->
-		<script nonce="rJGsCAAe">
-		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-		document,'script','https://connect.facebook.net/en_US/fbevents.js');
-
-		fbq('init', '417795779228010');
-		fbq('set','agent','tmgoogletagmanager', '417795779228010');
-		fbq('track', "PageView");
-		</script>
-		<noscript><img height="1" width="1" alt="" style="display:none"
-		src="https://www.facebook.com/tr?id=417795779228010&ev=PageView&noscript=1"
-		/></noscript>
-		<!-- End Facebook Pixel Code -->
 		<script type="text/javascript">
 		/*<![CDATA[*/
 		(function() {
@@ -57,13 +41,6 @@
 		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
 		})();
 		/*]]>*/
-		</script>
-		<script type="text/javascript">
-			(function(c,l,a,r,i,t,y){
-				c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-				t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-				y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-			})(window, document, "clarity", "script", "ayxhs2npkc");
 		</script>
 	</head>
 	<body <?php body_class(); ?>>
